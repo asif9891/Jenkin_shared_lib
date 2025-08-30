2e9358d4a489ac2f1924cc0ff61c1b798225fe44
asif9891 <95671092+asif9891@users.noreply.github.com>
2025-08-31 01:02:31 +0530

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -2,7 +2,6 @@ def buildFrontend(String tag) {
     withEnv(["TAG=${tag}"]) {
         sh """
           docker-compose build frontend
-          docker-compose up -d frontend
         """
     }
 }
